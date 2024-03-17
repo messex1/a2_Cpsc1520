6483449c360fef28d13de21be504d1fd7583381e
Michael Essex <112738703+messex1@users.noreply.github.com>
2024-03-17 13:56:05 -0600

    Assignment 2

--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
-# Assignment 2 Resources
-
-
-## The HTML Markup Template
-```html
- 
+/* 
 <tr>
   <td>ALBUM NAME HERE</td>
   <td>RELEASE DATE HERE</td>
@@ -11,6 +6,5 @@
   <td>GENRE HERE</td>
   <td>AVERAGE RATING HERE</td>
   <td>NUMBER OF RATINGS HERE</td>
-</tr>
- 
-```
+</tr> 
+*/
